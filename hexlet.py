ce57762c752f2de13b4a79812d3f4ca8caabf824
levus <levusachev082@outlook.com>
2022-10-25 19:07:30 +0400

    my first function on Python

--- hexlet.py
+++ hexlet.py
@@ -1,5 +1,13 @@
-# to learn use randint function
-from random import randint
-randomize_num = randint(1, 10)
-print(randomize_num)
+# define function which get string, get number of index in string and slice that and add
+# three dot in end of line
+def truncate(get_str, amount_char):
+    text = get_str[0:amount_char] + '...'
+    return text
 
+# define two variables which get string and index for function "truncate"
+some_text = 'Moya testovaya funcia'
+index = int(4)
+
+
+# Print result
+print(truncate(some_text, index))
